eee481f880bdf0b76fa185ce9161fb93a1c5797f
martapz <marta.g.mattioli@gmail.com>
2017-10-15 14:38:25 +0100

    added more logic

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
-function init() {
-  const $cells = $('li');
-  const $shapes = $('.choose-shapes')[0];
-  const $shape = $('.shape');
-  const $board = $('ul')[0];
-  const $winScreen = $('.win')[0];
-  const $winner = $('.win h2')[0];
-  const $playAgain = $('.win')[0];
-  let chosenShape = '';
-  let computerShape = '';
-  // const itsX = {isIt: '', shape: 'x'};
-  const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-  let corners = [0, 2, 6, 8];
-  let availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  const moves = [];
-  const userMoves = [];
-  const computerMoves = [];
-  // const movesHistoryX = [];
-  // const movesHistoryO = [];
-  let numberOfGames = 0;
-  let numberOfWins = 0;
-  let numberOfLosses = 0;
+const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+let corners = [0, 2, 6, 8];
+let sideMiddle = [1, 3, 5, 7];
+let availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const moves = [];
+const userMoves = [];
+const computerMoves = [];
+let chosenShape = '';
+let computerShape = '';
+let counter; // to check if user almost won
+// let numberOfGames = 0;
+// let numberOfWins = 0;
+// let numberOfLosses = 0;
 
-  function chooseShape(e) {
-    chosenShape = $(e.target).attr('value');
-    (chosenShape === 'x') ? computerShape = 'o' : computerShape = 'x';
-    $($board).css('display', 'block');
-    $($shapes).css('display', 'none');
+let $cells;
+let $shapes;
+let $shape;
+let $board;
+let $winScreen;
+let $winner;
+let $playAgain;
+
+function chooseShape(e) {
+  chosenShape = $(e.target).attr('value');
+  (chosenShape === 'x') ? computerShape = 'o' : computerShape = 'x';
+  $($board).css('display', 'block');
+  $($shapes).css('display', 'none');
+}
+
+function init() {
+  $cells = $('li');
+  $shapes = $('.choose-shapes')[0];
+  $shape = $('.shape');
+  $board = $('ul')[0];
+  $winScreen = $('.win')[0];
+  $winner = $('.win h2')[0];
+  $playAgain = $('.win')[0];
+
+  // const itsX = {isIt: '', shape: 'x'};
+
+
+  function checkIfComputerAlmostWon() {
+    for (var i = 0; i < wins.length; i++) {
+      counter = 0;
+      for (var j = 0; j < wins[i].length; j++) {
+        if (computerMoves.includes(wins[i][j])) {
+          counter ++;
+          if (counter === 2) {
+            console.log('yay Im about to win');
+            return checkWin(computerShape);
+          }
+        }
+      }
+      checkIfAlmostWon();
+    }
   }
 
   function checkIfAlmostWon() {
-    let counter = {};
     for (var i = 0; i < wins.length; i++) {
       counter = 0;
       for (var j = 0; j < wins[i].length; j++) {
@@ -54,15 +80,26 @@ function init() {
               // console.log(`AVAIL MOVES: (Computer turn) ${availableMoves}`);
               // console.log(`COMPUTER MOVES so far ${computerMoves}`);
             }
-            checkWin(computerShape);
+            // checkWin(computerShape);
+          } else {
+            if (corners.length > 1) {
+              const nextMove2 = corners[Math.floor(Math.random() * corners.length)];
+              moves.push({item: computerShape, position: nextMove2});
+              corners.splice(corners.indexOf(nextMove2), 1);
+              computerMoves.push(nextMove2);
+              return $($cells[nextMove2]).html(`<p>${computerShape}</p>`);
+            } else {
+              console.log('sidemiddle -->', sideMiddle);
+            }
           }
         }
       }
     }
+    checkWin(computerShape);
   }
 
   function computerTurn(numberTurn) {
-    if (numberTurn === 1) {
+    if (numberTurn === 1) { // if it's the first turn computer goes to random corner
       if (moves[moves.length - 1].position === 4) {
         const randomCorner = corners[Math.floor(Math.random() * corners.length)];
         const availIndexOfRandCorner = availableMoves.indexOf(randomCorner);
@@ -79,7 +116,9 @@ function init() {
         // console.log(`COMPUTER MOVES so far ${computerMoves}`);
       }
     } else {
-      checkIfAlmostWon();
+      console.log('computer moves', computerMoves);
+      checkIfComputerAlmostWon();
+      // checkIfAlmostWon();
     }
   }
 
@@ -89,6 +128,13 @@ function init() {
     userMoves.push(clickedItem);
     moves.push({item: chosenShape, position: clickedItem});
     availableMoves.splice(clickedItem, 1);
+    if (corners.includes(clickedItem)) {
+      console.log('user clicked on a corner');
+      corners.splice(corners.indexOf(clickedItem), 1);
+    }
+    if (sideMiddle.includes(clickedItem)) {
+      sideMiddle.splice(sideMiddle.indexOf(clickedItem), 1);
+    }
     // console.log('TOT MOVES (urser turn): -->', moves);
     // console.log(`AVAIL MOVES: (User turn) ${availableMoves}`);
     // console.log(`USER MOVES so far ${userMoves}`);
@@ -118,7 +164,9 @@ function init() {
   }
 
   function checkWin(shape) {
-    let counter = {};
+    console.log('how many moves so far --->', moves.length);
+    console.log('shape being checked --->', shape);
+    counter = {};
     for (var i = 0; i < wins.length; i++) {
       counter = { shape: shape, count: 0 };
       for (var j = 0; j < wins[i].length; j++) {
@@ -133,14 +181,15 @@ function init() {
         }
       }
     }
-    if (moves.length === 9) displayWinner('IT\'S A DRAW!');
+    if (moves.length === 9) return displayWinner('IT\'S A DRAW!');
   }
 
   function resetTable() {
-    moves.splice(0,moves.length);
+    moves.splice(0, moves.length);
     userMoves.splice(0, userMoves.length);
     computerMoves.splice(0, computerMoves.length);
     corners = [0, 2, 6, 8];
+    sideMiddle = [1, 3, 5, 7];
     availableMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     for (var i = 0; i < $cells.length; i++) {
       $($cells[i]).html('');
